6e8abf8b335e3f1963a5da3346bc6caaee1201e1
ajn313 <ajn313@nyu.edu>
2022-07-16 00:50:20 -0400

    Edit scripts for NVIDIA Hardware/XSEDE Environment

--- src/script/run-singularity.bash
+++ src/script/run-singularity.bash
@@ -9,13 +9,12 @@ echo $args
 ls
 if [ "$args" == "" ]; then args="/bin/bash"; fi
 
-if [[ "$(hostname -s)" =~ ^g[r,v,a] ]]; then nv="--nv"; fi
+
 
 singularity \
-  exec $nv \
-  --overlay /scratch/bf996/singularity_containers/openclip_env_cuda.ext3:ro \
-  --overlay /vast/work/public/ml-datasets/imagenet/imagenet-val.sqf:ro \
-  /scratch/work/public/singularity/cuda11.3.0-cudnn8-devel-ubuntu20.04.sif \
+  exec --nv \
+  --overlay /ocean/projects/cis220033p/ajn313/data/singularity/openclip_w0.ext3:ro \
+  /ocean/projects/cis220033p/ajn313/data/singularity/cuda11.3.0-cudnn8-devel-ubuntu20.04.sif \
   /bin/bash -c "
  source /ext3/env.sh
  $args 
