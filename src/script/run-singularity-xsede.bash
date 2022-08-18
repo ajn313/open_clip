fde8a27ab7d8ba9e079afbe6d5a450f427ba8374
André <79466926+ajn313@users.noreply.github.com>
2022-08-17 21:46:38 -0400

    Add files via upload

--- src/script/run-singularity-xsede.bash
+++ src/script/run-singularity-xsede.bash
@@ -9,11 +9,11 @@ echo $args
 ls
 if [ "$args" == "" ]; then args="/bin/bash"; fi
 
-if [[ "$(hostname -s)" =~ ^g[r,v,a] ]]; then nv="--nv"; fi
+
 
 singularity \
-  exec $nv \
-  --overlay /ocean/projects/cis220033p/ajn313/data/singularity/openclip_env_cuda.ext3:ro \
+  exec --nv \
+  --overlay /ocean/projects/cis220033p/ajn313/data/singularity/openclip_w0.ext3:ro \
   /ocean/projects/cis220033p/ajn313/data/singularity/cuda11.3.0-cudnn8-devel-ubuntu20.04.sif \
   /bin/bash -c "
  source /ext3/env.sh
